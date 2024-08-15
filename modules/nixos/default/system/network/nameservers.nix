f1a73e786aecb92297cc992956991e98b2c74aab
polarfill <71958117+PolarFill@users.noreply.github.com>
2024-08-14 21:57:50 -0300

    added option for dnscrypt to automatically choose best dns servers if cfg is enabled

--- modules/nixos/default/system/network/nameservers.nix
+++ modules/nixos/default/system/network/nameservers.nix
@@ -24,6 +24,12 @@
 	description = "Define dns protocol";
       };
 
+      ignore_server_list = lib.options.mkOption {
+	default = false;
+	type = lib.types.bool;
+	description = "Ignore cfg.servers if a custom protocol besides plaintext is used";
+      };
+
     };
 
   config = lib.mkIf cfg.enable {
@@ -50,11 +56,12 @@
     services.dnscrypt-proxy2 = lib.mkIf (cfg.protocol == "dnscrypt2") {
       enable = true;
       settings = {
-	listen_addresses = ["127.0.0.1:53" "[::1]:53"];
 	require_dnssec = true;
-	ipv6_servers = true;
+	require_nolog = true;
+	require_nofilter = true;
+      	ipv6_servers = true;
 	ignore_system_dns = true;
-	
+	listen_addresses = ["127.0.0.1:53" "[::1]:53"];
 
 	sources.public-resolvers = {
 	  urls = [
@@ -65,20 +72,20 @@
 	  minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
 	};
 
-	server_names = [
-	  ( if (builtins.elem "quad9" cfg.servers) then "quad9-dnscrypt-ip6-filter-pri" else "" )
+	server_names = if cfg.ignore_server_list == false then [
+	  ( if (builtins.elem "quad9" cfg.servers) then "quad9-dnscrypt-ip4-nofilter-ecs-pri" else "" )
 	  ( if (builtins.elem "opennic" cfg.servers) then "opennic1_kekew_info_ipv4" else "" )
 	  ( if (builtins.elem "opennic" cfg.servers) then "opennic1_kekew_info_ipv6" else "" )
 	  ( if (builtins.elem "opennic" cfg.servers) then "opennic2_kekew_info_ipv4" else "" )
 	  ( if (builtins.elem "opennic" cfg.servers) then "opennic3_kekew_info_ipv4" else "" )
-	];
+	] else [];
 
-	static = {
+	static = if cfg.ignore_server_list == false then {
 	  opennic1_kekew_info_ipv4 = { stamp = if (builtins.elem "opennic" cfg.servers) then "sdns://AQcAAAAAAAAAEDEwOS45MS4xODQuMjE6NTQgkuaRfQrPuzkvgT0_YGI_k5veCS_7H3cT4_Y3sgmop3oaMi5kbnNjcnlwdC1jZXJ0Lmtla2V3LmluZm8" else ""; };
 	  opennic1_kekew_info_ipv6 = { stamp = if (builtins.elem "opennic" cfg.servers) then "sdns://AQcAAAAAAAAAF1syMDAzOmE6NjRiOjNiMDA6OjhdOjU0IJLmkX0Kz7s5L4E9P2BiP5Ob3gkv-x93E-P2N7IJqKd6GjIuZG5zY3J5cHQtY2VydC5rZWtldy5pbmZv" else ""; };
 	  opennic2_kekew_info_ipv4 = { stamp = if (builtins.elem "opennic" cfg.servers) then "sdns://AQcAAAAAAAAAETgwLjE1Mi4yMDMuMTM0OjU0IPNqicy0t-MhnuiOf1kb4b42I1dkOyqw3NQ9VWhjjkbwGjIuZG5zY3J5cHQtY2VydC5rZWtldy5pbmZv" else ""; };
 	  opennic3_kekew_info_ipv4 = { stamp = if (builtins.elem "opennic" cfg.servers) then "sdns://AQcAAAAAAAAAF1syMDAzOmE6NjRiOjNiMDA6OjRdOjU0IP3tgRfQXIUOXFhC71k6nRR2G_bOr88lwFkjtO-iVgJOGjIuZG5zY3J5cHQtY2VydC5rZWtldy5pbmZv" else ""; };
-	};
+	} else {};
       };
     };
 
